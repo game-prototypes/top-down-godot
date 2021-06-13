4f972a5b23506ee7eeb489e2a83c9d1d853be709
angrykoala <angrykoala@outlook.es>
2021-06-13 20:31:08 +0200

    simple hitscan weapon

--- entities/player/bullet/bullet_trail.gd
+++
@@ -1,4 +0,0 @@
-extends Line2D
-
-func _on_animation_finished(anim_name):
-	queue_free()
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,12 +2,19 @@ extends Node2D
 
 export var speed:float=100
 
+export (PackedScene) var bullet
+
+onready var raycast:=$RayCast2D
+
 func _process(delta):
 	_movement_update(delta)
 	_look_update(delta)
-	
+
+
+
+func _physics_process(delta):
 	if Input.is_action_just_pressed("shoot"):
-		print("SHOOT")
+		_shoot()
 
 
 func _movement_update(delta):
@@ -24,5 +31,20 @@ func _movement_update(delta):
 	position += velocity * delta
 
 func _look_update(delta):
+	var target=_get_target_coordinates()
+	look_at(target)
+
+func _get_target_coordinates()->Vector2:
 	var mouse_position=get_viewport().get_mouse_position()
-	look_at(mouse_position)
+	return mouse_position
+
+func _shoot():
+	var bullet_instance=bullet.instance() as Bullet
+	var target=_get_target_coordinates()
+	
+	bullet_instance.set_path(position, target)
+	owner.add_child(bullet_instance)
+	raycast.cast_to=to_local(target)
+	raycast.force_raycast_update()
+	#print(position, target)
+	print(raycast.is_colliding())
